d52e96763624d6d8733844ce369b3adf2e28ad23
Gleedes Nazar <nazarurban2007@gmail.com>
2026-01-07 22:37:40 +0300

    files migrations

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -1,10 +1,12 @@
-DROP TABLE users; 
-
-DROP TABLE todo_lists;
+DROP TABLE lists_items;
 
 DROP TABLE user_lists;
 
+DROP TABLE todo_lists;
+
+DROP TABLE users; 
+
 DROP TABLE todo_items;
 
-DROP TABLE lists_items;
+
  
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,32 +1,35 @@
 CREATE TABLE users (
-    id SERIAL NOT NULL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL
 );
 
+
 CREATE TABLE todo_lists (
-    id SERIAL NOT NULL PRIMARY KEY UNIQUE,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255)
 );
 
+
 CREATE TABLE user_lists (
-    id SERIAL NOT NULL PRIMARY KEY UNIQUE,
-    user_id INT REFERENCES users(id) ON DELETE CASCADE NOT NULL,
-    list_id INT REFERENCES todo_lists(id) ON DELETE CASCADE NOT NULL
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    list_id INT NOT NULL REFERENCES todo_lists(id) ON DELETE CASCADE,
+    PRIMARY KEY (user_id, list_id)
 );
+
 
 CREATE TABLE todo_items (
-    id SERAIL NOT NULL PRIMARY KEY UNIQUE,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    desctiption VARCHAT(255)
-    done BOOLEAN NOT NULL DEFAULT FALSE,
+    description VARCHAR(255),
+    done BOOLEAN NOT NULL DEFAULT FALSE
 );
 
+
 CREATE TABLE lists_items (
-    id SERIAL NOT NULL PRIMARY KEY UNIQUE,
-    user_id INT REFERENCES todo_items(id) ON DELETE CASCADE NOT NULL,
-    list_id INT REFERENCES todo_lists(id) ON DELETE CASCADE NOT NULL
+    list_id INT NOT NULL REFERENCES todo_lists(id) ON DELETE CASCADE,
+    item_id INT NOT NULL REFERENCES todo_items(id) ON DELETE CASCADE,
+    PRIMARY KEY (list_id, item_id)
 );
- 
