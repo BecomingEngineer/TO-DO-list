b9ad5d6acb8198a580ea84c37875c2fffebe3f83
Gleedes Nazar <nazarurban2007@gmail.com>
2026-01-07 17:48:34 +0300

    configuring migrations

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,27 @@ import (
 	"Todolist/pkg/handler"
 	"Todolist/pkg/repository"
 	"Todolist/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 
 	handlers := handler.NewHandler(services)
 	srv := new(Todolist.Server)
-	if err := srv.Run("8000", handlers.InitRouters()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRouters()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
---
+++ schema/000001_init.down.sql
@@ -0,0 +1,12 @@
+DROP TABLE users; 
+
+CREATE TABLE users;
+
+CREATE TABLE todo_lists;
+
+CREATE TABLE user_lists;
+
+CREATE TABLE todo_items;
+
+CREATE TABLE lists_items;
+ 
