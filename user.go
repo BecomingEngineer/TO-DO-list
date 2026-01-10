56dfcf36318290f5932bb6add913dc86c3c73f2a
Gleedes Nazar <nazarurban2007@gmail.com>
2026-01-10 10:35:29 +0300

    fixed warning user.go

--- user.go
+++ user.go
@@ -2,7 +2,7 @@ package Todolist
 
 type User struct {
 	Id       int    `json:"-"`
-	Name     string `json:"name" binding :"required"`
-	Username string `json:"username" binding :"required"`
-	Password string `json:"password" binding :"required"`
+	Name     string `json:"name" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
